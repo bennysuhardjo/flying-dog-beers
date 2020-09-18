7bc102747e7db34f9996adaa150cedcfb2a351fb
bennysuhardjo <71486494+bennysuhardjo@users.noreply.github.com>
2020-09-18 16:04:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -106,10 +106,7 @@ app.layout = html.Div(children=[
         id='example-graph-2',
         figure=fig
     ),
-    dcc.Graph(
-        id='example-stock',
-        figure=figStock
-    ),
+
     html.A('Code on Github', href=githublink),
     html.Br(),
     html.A('Data Source', href=sourceurl),
