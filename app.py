a0fd18589a18a8af11b72a54bee3d0503961da0e
bennysuhardjo <71486494+bennysuhardjo@users.noreply.github.com>
2020-09-18 15:45:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,36 @@ beer_fig = go.Figure(data=beer_data, layout=beer_layout)
 
 
 data =requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=SPY&outputsize=full&apikey=L5W8DWNNL7QRMNH9').json()
+json_data = []
 
+for i in data['Time Series (Daily)']: 
+    #print(i, data['Time Series (Daily)'][i]) 
+    json_data.append( [i,data['Time Series (Daily)'][i]["1. open"],data['Time Series (Daily)'][i]["2. high"],
+                         data['Time Series (Daily)'][i]["3. low"], data['Time Series (Daily)'][i]["4. close"],
+                         data['Time Series (Daily)'][i]["5. adjusted close"],
+                         data['Time Series (Daily)'][i]["6. volume"],
+                         data['Time Series (Daily)'][i]["7. dividend amount"],
+                         data['Time Series (Daily)'][i]["8. split coefficient"]]) 
+
+
+df_all = pd.DataFrame.from_records( json_data )
+
+df_mod = df_all.rename(columns={0: "Date", 1: "Open", 2: "High", 3: "Low", 4: "Close", 5: "Adj Close", 6: "Volume", 7: "Dividend", 8: "Split Coefficient"})
+
+df_mod_2020 = df_mod[df_mod['Date'].str[:4] == "2020"]
+
+
+figStock = go.Figure(data=[go.Candlestick(x=df_mod_2020["Date"],
+                open=df_mod_2020["Open"],
+                high=df_mod_2020["High"],
+                low=df_mod_2020["Low"],
+                close=df_mod_2020["Close"])])
+figStock.update_xaxes(
+    rangebreaks=[
+        dict(bounds=["sat", "mon"]), #hide weekends
+        dict(values=["2015-12-25", "2016-01-01"])  # hide Christmas and New Year's
+    ]
+)
 
 
 ########### Initiate the app
@@ -76,6 +105,10 @@ app.layout = html.Div(children=[
     dcc.Graph(
         id='example-graph-2',
         figure=fig
+    ),
+    dcc.Graph(
+        id='example-stock',
+        figure=figStock
     ),
     html.A('Code on Github', href=githublink),
     html.Br(),
