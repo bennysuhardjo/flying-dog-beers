5cce17d9bb67b1f96abc85dbe98040e2e3d8f7e6
bennysuhardjo <71486494+bennysuhardjo@users.noreply.github.com>
2020-09-22 18:11:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import plotly.express as px
 from dash.dependencies import Input, Output, State
 import time
 import dash_table
-
+import zipfile, urllib.request, shutil
+import dash_bootstrap_components as dbc
 
 ########### Define your variables
 mytitle='Stock Trend'
@@ -30,11 +31,80 @@ def generate_table(dataframe, max_rows=10):
     ])
 
 
+headers = {'AccountKey': '/cwI6AoOQzefPZARL5M4Eg==',
+           'accept': 'application/json'
+}
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
+results = []
+
+while True:
+     new_results = requests.get(
+         "http://datamall2.mytransport.sg/ltaodataservice/BusRoutes",
+         headers=headers,
+         params={'$skip': len(results)}
+     ).json()['value']
+     if new_results == []:
+         break
+     else:
+         results += new_results
+
+
+
+#data =requests.get("http://datamall2.mytransport.sg/ltaodataservice/BusRoutes", headers=headers).json()
+#data
+
+json_data = []
+
+#data['value']
+
+for i in results: 
+    #print(i, i["BusStopCode"]) 
+    json_data.append( [i["ServiceNo"],i["Operator"],
+                       i["Direction"],i["StopSequence"],
+                       i["BusStopCode"],i["Distance"],
+                       i["WD_FirstBus"],i["WD_LastBus"],
+                       i["SAT_FirstBus"],i["SAT_LastBus"],
+                       i["SUN_FirstBus"],i["SUN_LastBus"]
+
+                      
+                      
+                      ]) 
+
+
+df_busRoute = pd.DataFrame.from_records( json_data ).rename(columns={0: "ServiceNo", 1: "Operator", 2: "Direction", 3: "StopSequence", 4: "BusStopCode"
+                                                                     , 5: "Distance", 6: "WD_FirstBus", 7: "WD_LastBus", 8: "SAT_FirstBus", 9: "SAT_LastBus"
+                                                                    , 10: "SUN_FirstBus", 11: "SUN_LastBus"})
+
+headers = {'AccountKey': 'NQD/ZccwR5SEDj/MehpKww== ',
+           'accept': 'application/json'
+}
+# MPHd2F+USH+1hA1FDikGeA==
+# NQD/ZccwR5SEDj/MehpKww== 
+
+r =requests.get("http://datamall2.mytransport.sg/ltaodataservice/PV/Bus", headers=headers).json()
+
+
+url = r['value'][0]['Link']
+file_name = 'myzip.zip'
+
+with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+    shutil.copyfileobj(response, out_file)
+    with zipfile.ZipFile(file_name) as zf:
+        zf.extractall()
+df_busStopStats = pd.read_csv(file_name)
+df_busStopStatsSummary = df_busStopStats.groupby(['DAY_TYPE','TIME_PER_HOUR'], as_index=False)[["TOTAL_TAP_IN_VOLUME"]].sum()
+
+fig = go.Figure(data=go.Heatmap(
+                   z=df_busStopStatsSummary['TOTAL_TAP_IN_VOLUME'],
+                   x=df_busStopStatsSummary['TIME_PER_HOUR'],
+                  y=df_busStopStatsSummary['DAY_TYPE'],
+                   hoverongaps = False))
+fig.update_layout(
+    title="Tap-In Volume (Bus) by Hours of the Day"
+)
 
 ########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = [dbc.themes.BOOTSTRAP]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title=tabtitle
@@ -43,7 +113,7 @@ app.title=tabtitle
 app.layout = html.Div([
     dcc.Tabs([
         dcc.Tab(label='Financial Market', children=[
-            
+            html.H1(myheading),
 
             html.Label('US Stock Ticker: '),
             dcc.Input(id='stock_ticker', value='IBM', type='text'),
@@ -74,16 +144,23 @@ app.layout = html.Div([
             ])
        ]),
        dcc.Tab(label='Public Transport', children=[
-            dcc.Graph(
-                figure={
-                    'data': [
-                        {'x': [1, 2, 3], 'y': [1, 4, 1],
-                            'type': 'bar', 'name': 'SF'},
-                        {'x': [1, 2, 3], 'y': [1, 2, 3],
-                         'type': 'bar', 'name': u'Montréal'},
-                    ]
-                }
-            )
+		dbc.Row([
+            		dbc.Col(
+	    			dcc.Graph(
+                			figure={
+                    				'data': [{'x': df_busRoute['Operator'],
+                    		 			  'type': 'histogram'},
+                    					],
+						'layout': {'title': 'No of Buses by Operators'}
+                			}
+            			), width=4
+			),
+	    		dbc.Col(
+	    			dcc.Graph(
+                			figure=fig
+            			), width=8)
+	    	])
+	    	
         ]),
         dcc.Tab(label='Banking', children=[
             dcc.Graph(
