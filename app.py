ac75756e325841e141f07cb1a7fa854bf70430de
bennysuhardjo <71486494+bennysuhardjo@users.noreply.github.com>
2020-09-23 20:47:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 # Open a cursor to perform database operations
 cur = conn.cursor()
 # Execute a query
-cur.execute("SELECT * FROM Transaction");
+cur.execute("SELECT * FROM LTA.Transaction");
 # Retrieve query results
 df_busStopStats = cur.fetchall()
 
